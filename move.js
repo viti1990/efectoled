652702695b3de43df6e47691f9811ab72b0ce3fb
Víctor Ortuño <ortuno20@hotmail.com>
2020-06-13 19:00:44 +0200

    todo de crem

--- move.js
+++ move.js
@@ -48,7 +48,121 @@ window.addEventListener("load",function(){
 
 
 
+            var edad = '';
+            var color = '';
+            var dia = '';
 
+
+                $(".item1__btn").click(function(){
+                    edad = $(".item1__input").val();
+
+                    if(edad != ''){
+                       $(".datos").hide()
+                       $(".item1").slideUp()
+                       $(".item2").show()
+                    }
+                    else{
+
+                        $(".datos").html("ESCRIBE ALGO CANELO")
+                    }
+
+
+
+                })
+
+
+            $(".item2__btn").click(function(){
+                color = $(".item2__input").val();
+                if(color != ''){
+                    $(".datos").hide()
+                    $(".item2").slideUp()
+                   $(".item4").show()
+                }
+
+                else{
+
+                    $(".datos").html("ESCRIBE ALGO CANELO")
+                    $(".datos").show()
+                }
+
+
+            })
+
+            $(".siguiente").click(function () {
+                $(".item3").show()
+                $(".item4").slideUp()
+                $(".enviartodo").show()
+            })
+
+            $("form").submit(function(e){
+                e.preventDefault()
+                dia = $(".item3__input").val();
+
+                if(dia != ''){
+                    $(".datos").hide;
+
+                      var datos=$(".formu").serialize()
+
+    console.log(datos)
+
+
+                    $(".datos").html(`
+               <p>el numero es ${edad}</p>
+              <p>el color es  ${color}</p>
+               <p>el dia es ${dia}</p>
+            
+            
+            `)
+
+                    $(".datos").html(datos)
+                $(".datos").show()
+
+
+                    $.ajax({
+                        type:'POST',
+                        url:'https://reqres.in/api/users',
+                        data:datos,
+                        success:function(response){
+                            console.log(response);
+
+                            setTimeout(function(){
+                                $(".datos").append('ENVIO CON EXITO')
+
+
+                            },3000)
+
+
+                        },
+
+
+                        error:function () {
+                            alert("hubo un error")
+                        }
+
+                    })
+
+
+                }
+
+                else{
+                    $(".datos").html("ESCRIBE ALGO CANELO")
+                    $(".datos").show()
+                }
+
+
+
+            })
+
+
+/*
+
+            $( "form" ).on( "submit", function( event ) {
+                event.preventDefault();
+                var resultados = $(this).serialize()
+                console.log(resultados);
+            });
+
+*/
         })
 
 
